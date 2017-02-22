ce78a93bdb5d06f239b713889a80bf4eab772477
ShaneN <shane94@hotmail.com>
2017-02-21 23:54:14 -0700

    First checkin of repro project

---
+++ TaskToObservables/TaskToObservables/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TaskToObservables
+{
+	public partial class App : Application
+	{
+
+        static NavigationPage page = null;
+
+        public static IObservable<Unit> PopEverythingAndSetALabel()
+        {
+            return page.PopToRootAsync().ToObservable()
+                .Do(_ =>
+                {
+                    try
+                    {
+                        (page.CurrentPage as MainPage).SetLabel();
+                    }
+                    catch(Exception exc)
+                    {
+                        (page.CurrentPage as MainPage).SetException(exc.ToString());
+                    }
+                    
+                })
+                .Select(_=> Unit.Default);
+        }
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            page = new NavigationPage(new TaskToObservables.MainPage());
+
+            page.PushAsync(new TaskToObservables.MainPage()).Wait();
+            page.PushAsync(new TaskToObservables.MainPage()).Wait();
+            page.PushAsync(new TaskToObservables.MainPage()).Wait();
+            page.PushAsync(new TaskToObservables.MainPage()).Wait();
+            page.PushAsync(new TaskToObservables.MainPage()).Wait();
+
+            MainPage = page;
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ TaskToObservables/TaskToObservables/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TaskToObservables
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Observable.FromEventPattern<EventHandler, EventArgs>(
+                x => BOOM.Clicked += x,
+                x => BOOM.Clicked -= x
+                )
+            .SelectMany(_=> App.PopEverythingAndSetALabel())
+            .Subscribe();
+        }
+
+        public void SetException(string error)
+        {
+            Observable.Start(() =>
+            {
+                lblError.Text = error;
+                lblError.TextColor = Color.Red;
+            },RxApp.MainThreadScheduler
+            ).Subscribe();
+            
+        }
+
+        public void SetLabel()
+        {
+#if __IOS__
+            someLabel.Text = $"{Guid.NewGuid()}";
+#else
+            someLabel.Text = $"IsThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread} - {Guid.NewGuid()} ";
+#endif
+
+            lblError.Text = String.Empty;
+
+        }
+    }
+}
